249dedfbf72354a0a3b400b5e42ef1028cb17812
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2025-01-05 20:07:02 +0100

    Added Routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import express from "express";
 //dotenv - extension for env file
 import dotenv from "dotenv";
 
+
+//safe routes
 import authRoutes from "./routes/auth.route.js";
 
 //function to start connecting with env file
@@ -17,6 +19,7 @@ const app = express();
 // environment variables: read port 5000 if env file undefined
 const PORT = process.env.PORT || 5000;
 
+//path to singup: /api/auth/signup
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
