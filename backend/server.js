63f3d0a0f63233d8fb8168529935ac0a40fa5ae2
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2025-01-05 18:13:02 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+//npm run dev - for development
+// npm run start - for production
+
+import express from "express";
+
+//dotenv - extension for env file
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+
+//function to start connecting with env file
+dotenv.config();
+
+//function app start
+const app = express();
+
+// environment variables: read port 5000 if env file undefined
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
