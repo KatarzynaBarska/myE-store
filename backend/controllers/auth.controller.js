8b84f856e68f7fe632c601381cce04f4104a223e
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2025-01-12 20:38:26 +0100

    Added Cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 //obsługa ścieżek
-
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+
 
 dotenv.config();
 
@@ -14,11 +15,28 @@ const generateTokens = (userId) => {
     const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: "7d"});
     
     return {accessToken, refreshToken};
-}
+};
 
 //funkcja do zapisania utworzonych tokenów w bazie danych Redis
 const storeRefreshToken = async (userId, refreshToken) => {
-    await Redis.set(`refreshToken:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7 dni
+    await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7 dni
+}
+
+//funkcja do zapisania Cookies - deklaracja
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // prevent  XSS attacks
+        secure:process.env.NODE_ENV !== "development", // only works in production
+        sameSite: "strict", // prevent SCRF attack, cross-site request forgery
+        maxAge: 15 * 60 * 1000, // 15 minutes
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // prevent  XSS attacks
+        secure:process.env.NODE_ENV !== "development", // only works in production
+        sameSite: "strict", // prevent SCRF attack, cross-site request forgery
+        maxAge: 7 *15 * 60 * 1000, // 7 days
+    })
 }
 
 
@@ -41,7 +59,16 @@ const { accessToken, refreshToken } = generateTokens(user._id);
 // Destrukturyzacja obiektu i wyciągnięcie z niego accessToken i refreshToken
 await storeRefreshToken(user._id, refreshToken);
 
-    res.status(201).json({message:"User created successfully"});
+//funkcja do zapisywanie Cookies
+setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+        user: {
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        }, message:"User created successfully"});
     } catch (error) {
         res.status(500).json({message:error.message});
     }
