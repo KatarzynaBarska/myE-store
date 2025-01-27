8ed8d3e87ba0cd66d8456be283db7a06282f18cb
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2025-01-27 19:58:50 +0100

    Added protectet route

--- backend/routes/product.routs.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-
-const router = express.Router();
-
-router.get("/", protectRoute, adminRoute, getAllProducts);
-
-export default router;
