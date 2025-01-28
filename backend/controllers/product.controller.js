20b20e5a8e617cda688fa78bf4c75a282d74c8e3
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2025-01-28 18:20:50 +0100

    Added getFeaturedProducts function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,9 +1,36 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
     try {
         const products = await Product.find({}); //find all products
         res.json({products});
+    } catch (error) {
+        console.log("Error in get all products controller", error.message);
+        res.status(500).json({message: "Server error", error:error.message});
+        
+    }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featuredProducts"); //find all products
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+         //if not in redis, fetch from mongodb
+        featuredProducts = await Product.find({isFeatured: true}).lean();
+
+        if (!featuredProducts) {
+
+            return res.status(404).json({message: "No featured products found"});
+
+        }
+
+        // store in redis for future quick access
+
+        await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+
     } catch (error) {
         console.log("Error in get all products controller", error.message);
         res.status(500).json({message: "Server error", error:error.message});
